75f0ce293cd4ef58fd0fc5a412c65fb4cecb0c7f
Thuy Nguyen <tnguyen@cfenet.ubc.ca>
2015-08-11 13:13:14 -0700

    Remove unnecessary line

--- run_HCVDeli.sh
+++ run_HCVDeli.sh
@@ -5,8 +5,6 @@ quality=0
 
 for run in $runs; do
 
-mpirun --machinefile ~/gitrepo/mixed-hcv/out/machines.txt --output-filename ~/gitrepo/mixed-hcv/log/deli/HCVDeli.log python HCVDeli.py -pathlist ~/gitrepo/mixed-hcv/reads/allreads.txt -log ~/gitrepo/mixed-hcv/log/deli/HCVDeli.mpi.log -x ~/gitrepo/mixed-hcv/data/gb-ref+hg38_v2 ~/gitrepo/mixed-hcv/out/deli/HCVDeli.mpi.out.csv
-
     mpirun -hostfile ./out/machines.txt -output-filename ./log/$run/$run.deli.stdout.log -v python HCVDeli.py  -path ./reads/$run  -log ./log/$run/$run.deli.log -x data/gb-ref+hg38_v2 -minq $quality  ./out/$run/$run.deli.csv
 
     bash catcsv.sh ./out/$run/$run.deli.csv.* >  /media/macdatafile/mixed-hcv/gb-ref+hg38_v2/deli/$run.csv

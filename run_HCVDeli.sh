28c19ef576036a05fb33dd62fd304d1cd19d6fd3
Thuy Nguyen <tnguyen@cfenet.ubc.ca>
2015-08-18 17:35:11 -0700

    Use new standard naming convention.  Specify sam cache.

--- run_HCVDeli.sh
+++ run_HCVDeli.sh
@@ -5,16 +5,18 @@ quality=0
 minwidth=0.5
 ref=gb-ref+hg38_v2
 
+
 for run in $runs; do
 
     mkdir -p ./log/$run
     mkdir -p ./out/$run
 
-    mpirun -hostfile ./out/machines.txt -output-filename ./log/$run/q$quality.$ref.minwidth$minwidth.$run.deli.stdout.log -v python HCVDeli.py  -path ./reads/$run  -log ./log/$run/q$quality.$ref.minwidth$minwidth.$run.deli.log -x data/$ref -minq $quality  -min_target_width $minwidth ./out/$run/q$quality.$ref.minwidth$minwidth.$run.deli.csv
 
-    bash catcsv.sh ./out/$run/q$quality.$ref.minwidth$minwidth.$run.deli.csv.* >  /media/macdatafile/mixed-hcv/gb-ref+hg38_v2/deli/q$quality.$ref.minwidth$minwidth.$run.csv
+    mpirun -hostfile ./out/machines.txt --cache /media/macdatafile/mixed-hcv/cache/ -output-filename ./log/$run/$run__deli__HCVhuman__q$quality__mw$minwidth.stdout.log -v python HCVDeli.py  -path ./reads/$run  -log ./log/$run/$run__deli__HCVhuman__q$quality__mw$minwidth.log -x data/$ref -minq $quality  -min_target_width $minwidth ./out/$run/$run__deli__HCVhuman__q$quality__mw$minwidth.csv
+
+    bash catcsv.sh ./out/$run/$run__deli__HCVhuman__q$quality__mw$minwidth.csv.* >  /media/macdatafile/mixed-hcv/gb-ref+hg38_v2/deli/$run__deli__HCVhuman__q$quality__mw$minwidth.csv
 
 
-    Rscript launch_knitr_report.R  subtype_slice_perc.R  /media/macdatafile/mixed-hcv/gb-ref+hg38_v2/deli/q$quality.$ref.minwidth$minwidth.$run.csv  /media/macdatafile/mixed-hcv/expected_mixture/$run.expected_mixture.csv  q$quality.$ref.minwidth$minwidth.$run
+    Rscript launch_knitr_report.R  subtype_slice_perc.R  /media/macdatafile/mixed-hcv/gb-ref+hg38_v2/deli/$run__deli__HCVhuman__q$quality__mw$minwidth.csv  /media/macdatafile/mixed-hcv/expected_mixture/$run.expected_mixture.csv  $run__deli__HCVhuman__q$quality__mw$minwidth.csv
 
 done;

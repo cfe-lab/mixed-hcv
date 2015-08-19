78f855268a2bb6d82f1cfdc7e4f87e9ca4e47316
Thuy Nguyen <tnguyen@cfenet.ubc.ca>
2015-08-18 17:45:22 -0700

    Loop over multiple qualities, minwidths.

--- run_HCVDeli.sh
+++ run_HCVDeli.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
 runs=$@
-quality=0
-minwidth=0.5
+qualities=(0 10)
+minwidths=(0.25 0.5 0.75 1.0)
 ref=gb-ref+hg38_v2
-
+refnice=$refnice
 
 for run in $runs; do
 
     mkdir -p ./log/$run
     mkdir -p ./out/$run
 
+    for quality in $qualities; do
+        for minwidth in minwidths; do
+            mpirun -hostfile ./out/machines.txt  -output-filename ./log/$run/$run__deli__$refnice__q$quality__mw$minwidth.stdout.log -v python HCVDeli.py  -path ./reads/$run  --cache /media/macdatafile/mixed-hcv/cache/ -log ./log/$run/$run__deli__$refnice__q$quality__mw$minwidth.log -x data/$ref -minq $quality  -min_target_width $minwidth ./out/$run/$run__deli__$refnice__q$quality__mw$minwidth.csv
 
-    mpirun -hostfile ./out/machines.txt --cache /media/macdatafile/mixed-hcv/cache/ -output-filename ./log/$run/$run__deli__HCVhuman__q$quality__mw$minwidth.stdout.log -v python HCVDeli.py  -path ./reads/$run  -log ./log/$run/$run__deli__HCVhuman__q$quality__mw$minwidth.log -x data/$ref -minq $quality  -min_target_width $minwidth ./out/$run/$run__deli__HCVhuman__q$quality__mw$minwidth.csv
-
-    bash catcsv.sh ./out/$run/$run__deli__HCVhuman__q$quality__mw$minwidth.csv.* >  /media/macdatafile/mixed-hcv/gb-ref+hg38_v2/deli/$run__deli__HCVhuman__q$quality__mw$minwidth.csv
+            bash catcsv.sh ./out/$run/$run__deli__$refnice__q$quality__mw$minwidth.csv.* >  /media/macdatafile/mixed-hcv/gb-ref+hg38_v2/deli/$run__deli__$refnice__q$quality__mw$minwidth.csv
 
 
-    Rscript launch_knitr_report.R  subtype_slice_perc.R  /media/macdatafile/mixed-hcv/gb-ref+hg38_v2/deli/$run__deli__HCVhuman__q$quality__mw$minwidth.csv  /media/macdatafile/mixed-hcv/expected_mixture/$run.expected_mixture.csv  $run__deli__HCVhuman__q$quality__mw$minwidth.csv
-
+            Rscript launch_knitr_report.R  subtype_slice_perc.R  /media/macdatafile/mixed-hcv/gb-ref+hg38_v2/deli/$run__deli__$refnice__q$quality__mw$minwidth.csv  /media/macdatafile/mixed-hcv/expected_mixture/$run.expected_mixture.csv  $run__deli__$refnice__q$quality__mw$minwidth.csv
+        done;
+    done;
 done;

bee74422c3b410bb5814c4f0725deba9e1ce8761
Thuy Nguyen <tnguyen@cfenet.ubc.ca>
2015-08-19 11:04:21 -0700

    Actually specify user-friendly reference name, echo parameters, escape variable names with curly brackets so that underscores in filenames are not confused as part of the variable name

--- run_HCVDeli.sh
+++ run_HCVDeli.sh
@@ -4,7 +4,7 @@ runs=$@
 qualities=(0 10)
 minwidths=(0.25 0.5 0.75 1.0)
 ref=gb-ref+hg38_v2
-refnice=$refnice
+refnice=HCVhuman
 
 for run in $runs; do
 
@@ -12,13 +12,14 @@ for run in $runs; do
     mkdir -p ./out/$run
 
     for quality in $qualities; do
-        for minwidth in minwidths; do
-            mpirun -hostfile ./out/machines.txt  -output-filename ./log/$run/$run__deli__$refnice__q$quality__mw$minwidth.stdout.log -v python HCVDeli.py  -path ./reads/$run  --cache /media/macdatafile/mixed-hcv/cache/ -log ./log/$run/$run__deli__$refnice__q$quality__mw$minwidth.log -x data/$ref -minq $quality  -min_target_width $minwidth ./out/$run/$run__deli__$refnice__q$quality__mw$minwidth.csv
+        for minwidth in $minwidths; do
+        	echo "run=$run quality=$quality minwidth=$minwidth ref=$ref refnice=$refnice"
+            mpirun -hostfile ./out/machines.txt  -output-filename ./log/$run/${run}__deli__${refnice}__q${quality}__mw${minwidth}.stdout.log -v python HCVDeli.py  -path ./reads/$run  --cache /media/macdatafile/mixed-hcv/cache/ -log ./log/$run/${run}__deli__${refnice}__q${quality}__mw${minwidth}.log -x data/$ref -minq $quality  -min_target_width $minwidth ./out/$run/${run}__deli__${refnice}__q${quality}__mw${minwidth}.csv
 
-            bash catcsv.sh ./out/$run/$run__deli__$refnice__q$quality__mw$minwidth.csv.* >  /media/macdatafile/mixed-hcv/gb-ref+hg38_v2/deli/$run__deli__$refnice__q$quality__mw$minwidth.csv
+            bash catcsv.sh ./out/$run/${run}__deli__${refnice}__q${quality}__mw${minwidth}.csv.* >  /media/macdatafile/mixed-hcv/gb-ref+hg38_v2/deli/${run}__deli__${refnice}__q${quality}__mw${minwidth}.csv
 
 
-            Rscript launch_knitr_report.R  subtype_slice_perc.R  /media/macdatafile/mixed-hcv/gb-ref+hg38_v2/deli/$run__deli__$refnice__q$quality__mw$minwidth.csv  /media/macdatafile/mixed-hcv/expected_mixture/$run.expected_mixture.csv  $run__deli__$refnice__q$quality__mw$minwidth.csv
+            Rscript launch_knitr_report.R  subtype_slice_perc.R  /media/macdatafile/mixed-hcv/gb-ref+hg38_v2/deli/${run}__deli__${refnice}__q${quality}__mw${minwidth}.csv  /media/macdatafile/mixed-hcv/expected_mixture/$run.expected_mixture.csv  ${run}__deli__${refnice}__q${quality}__mw${minwidth}.csv
         done;
     done;
 done;

c2e3578fd9a98f1de3591e7935d7234133c625cd
Thuy Nguyen <tnguyen@cfenet.ubc.ca>
2015-08-20 18:29:44 -0700

    add deli runtype

--- run_HCVDeli.sh
+++ run_HCVDeli.sh
@@ -17,7 +17,7 @@ for run in ${runs[@]}; do
             mpirun -hostfile ./out/machines.txt  -output-filename ./log/$run/${run}__deli__${refnice}__q${quality}__mw${minwidth}.stdout.log -v python HCVDeli.py  -path ./reads/$run  --cache /media/macdatafile/mixed-hcv/cache/ -log ./log/$run/${run}__deli__${refnice}__q${quality}__mw${minwidth}.log -x data/$ref -minq $quality  -min_target_width $minwidth ./out/$run/${run}__deli__${refnice}__q${quality}__mw${minwidth}.csv
 
 
-            python collectcat.py -runname ${run} -output /media/macdatafile/mixed-hcv/staging/ -x ${ref} -minq ${quality} -min_target_width ${minwidth} --cache /media/macdatafile/mixed-hcv/cache/ ./out/$run/${run}__deli__${refnice}__q${quality}__mw${minwidth}.csv.*
+            python collectcat.py -runname ${run} -output /media/macdatafile/mixed-hcv/staging/ -runtype deli -x ${ref} -minq ${quality} -min_target_width ${minwidth} --cache /media/macdatafile/mixed-hcv/cache/ ./out/$run/${run}__deli__${refnice}__q${quality}__mw${minwidth}.csv.*
 
 
             Rscript launch_knitr_report.R  subtype_slice_perc.R  /media/macdatafile/mixed-hcv/mixture_reports/${run}__deli__${refnice}__q${quality}__mw${minwidth}.subtype_slice_perc.html  /media/macdatafile/mixed-hcv/staging/$run/${run}__deli__${refnice}__q${quality}__mw${minwidth}.csv  /media/macdatafile/mixed-hcv/expected_mixture/$run.expected_mixture.csv  ${run}__deli__${refnice}__q${quality}__mw${minwidth}

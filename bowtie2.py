fac6d989813803c96d31f12dd4d63006f29cd1a1
don <donkirkby@users.noreply.github.com>
2017-07-28 14:09:34 -0700

    Bowtie2 fix by Scotty that never got committed to GitHub.
    Change was made on Kive on 29 Mar 2017.

--- bowtie2.py
+++ bowtie2.py
@@ -1,5 +1,6 @@
 """
 Wrapper script for bowtie2-align and bowtie2-build with version control
+VERSION by Scotty for improved bowtie2 version checking
 """
 import subprocess
 from itertools import izip_longest
@@ -12,15 +13,23 @@ def build(version, fasta):
     :param fasta: Path to FASTA containing reference sequences
     :return:
     """
+    base_str = 'bowtie2-build'
+    version_str = '%s-%s' % (base_str, version)
+    build_cmd = version_str
     try:
-        subprocess.check_output(['bowtie2-build', '-h'])
+        subprocess.check_output([build_cmd, '-h'])
     except OSError:
-        raise RuntimeError('bowtie2-build not found; check if it is installed and in $PATH\n')
+        build_cmd = base_str
+        try:
+            subprocess.check_output([build_cmd, '-h'])
+        except:
+            raise RuntimeError("""Neither '%s' nor '%s' found;
+check if at least one of these is installed and in $PATH\n""" % (version_str, base_str))
 
-    stdout = subprocess.check_output(['bowtie2-build', '--version'])
+    stdout = subprocess.check_output([build_cmd, '--version'])
     local_version = stdout.split('\n')[0].split()[-1]
     assert version == local_version, 'bowtie2-build version incompatibility %s != %s' % (version, local_version)
-    subprocess.check_call(['bowtie2-build', '-q', '-f', fasta, fasta])
+    subprocess.check_call([build_cmd, '-q', '-f', fasta, fasta])
 
 
 def align_paired(version, refpath, fastq1, fastq2, nthreads, flags=('--quiet', '--no-unal', '--local')):
@@ -33,20 +42,27 @@ def align_paired(version, refpath, fastq1, fastq2, nthreads, flags=('--quiet', '
     :param flags: A tuple of bowtie2 flags such as --local
     :return:
     """
-
     # check that we have access to bowtie2
+    base_str = 'bowtie2'
+    version_str = '%s-%s' % (base_str, version)
+    bt2_cmd = version_str
     try:
-        subprocess.check_output(['bowtie2', '-h'])
+        subprocess.check_output([bt2_cmd, '-h'])
     except OSError:
-        raise RuntimeError('bowtie2 not found; check if it is installed and in $PATH\n')
+        bt2_cmd = base_str
+        try:
+            subprocess.check_output([bt2_cmd, '-h'])
+        except:
+            raise RuntimeError("""Neither '%s' nor '%s' found;
+check if at least one of these is installed and in $PATH\n""" % (version_str, base_str))
 
     # check that version is the expected version
-    stdout = subprocess.check_output(['bowtie2', '--version'])
+    stdout = subprocess.check_output([bt2_cmd, '--version'])
     local_version = stdout.split('\n')[0].split()[-1]
     assert version == local_version, 'bowtie2 version incompatibility %s != %s' % (version, local_version)
 
     # stream output from bowtie2, two at a time
-    bowtie_args = ['bowtie2', '-x', refpath, '-1', fastq1, '-2', fastq2, '-p', str(nthreads)] + list(flags)
+    bowtie_args = [bt2_cmd, '-x', refpath, '-1', fastq1, '-2', fastq2, '-p', str(nthreads)] + list(flags)
     p2 = subprocess.Popen(bowtie_args, stdout=subprocess.PIPE)
     for line, line2 in izip_longest(p2.stdout, p2.stdout, fillvalue=''):
         yield line, line2
@@ -58,7 +74,7 @@ def align_paired(version, refpath, fastq1, fastq2, nthreads, flags=('--quiet', '
 
 
 def main():
-    iter = align_paired('2.1.3', 'gb-ref.fa', 'test1.fastq', 'test2.fastq')
+    iter = align_paired('2.2.8', 'gb-ref.fa', 'test1.fastq', 'test2.fastq', 1)
     for line in iter:
         print line
 
